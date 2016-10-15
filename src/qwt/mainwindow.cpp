c8751ed8459d2fc46c7cbfafe2cc49b48cb243a8
sjrdc <scrijns@gmail.com>
2016-10-15 22:07:05 +0200

    loose the stylesheet

--- src/qwt/mainwindow.cpp
+++ src/qwt/mainwindow.cpp
@@ -1,19 +1,16 @@
 #include "mainwindow.h"
 #include "plot.h"
-#include <qmath.h>
 
+#include <qmath.h>
 
 MainWindow::MainWindow()
 {
-    d_plot = new Plot( this );
-	d_plot->setTitle( "Scatter Plot" );
-    setCentralWidget( d_plot );
-
-    // a million points
-	setSamples( 10000 );
+  d_plot = new Plot(this);
+  d_plot->setTitle("Scatter Plot");
+  setCentralWidget(d_plot);
 }
 
-void MainWindow::setSamples(const QPolygonF &samples )
+void MainWindow::setSamples(const QPolygonF &samples)
 {
-  d_plot->setSamples( samples );
+  d_plot->setSamples(samples);
 }
