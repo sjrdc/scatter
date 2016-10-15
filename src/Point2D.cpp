13e8d07aff988f35e3fb17cb37f37eecd8d3685c
sjrdc <scrijns@gmail.com>
2016-10-15 17:09:20 +0200

    add basic stuff

---
+++ src/Point2D.cpp
@@ -0,0 +1,8 @@
+#include "Point2D.h"
+
+Point2D::Point2D(int x, int y)
+{
+  x_ = x;
+  y_ = y;
+}
+
---
+++ src/Point2D.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Point2D
+{
+public:
+  Point2D(int x, int y);
+  int x_;
+  int y_;
+};
