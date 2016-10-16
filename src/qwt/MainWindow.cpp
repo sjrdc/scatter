e85323eec718e0e96e80673ab9d7bcf9acc544e4
sjrdc <scrijns@gmail.com>
2016-10-16 18:07:55 +0200

    move reading infile to MainWindow class

--- src/qwt/MainWindow.cpp
+++ src/qwt/MainWindow.cpp
@@ -1,13 +1,35 @@
 #include "MainWindow.h"
 #include "Plot.h"
 
+#include <fstream>
+#include <iostream>
+
 #include <qmath.h>
 
-MainWindow::MainWindow(QString title)
+MainWindow::MainWindow(QString infile)
 {
   d_plot = new Plot(this);
-  d_plot->setTitle(title);
+  d_plot->setTitle(infile);
   setCentralWidget(d_plot);
+
+  // start reading input file
+  std::ifstream inputstream;
+  inputstream.open(infile.toStdString().c_str());
+  if (!inputstream.is_open())
+    {
+      std::cerr << "error - could not open file "
+		<< infile.toStdString() << "\n";
+    }
+
+  // read points from file
+  float x, y;
+  int counter;
+  QPolygonF samples;
+  while (inputstream >> counter >> x >> y)
+    {
+      samples += QPointF(x, y);
+    }
+  setSamples(samples);
 }
 
 void MainWindow::setSamples(const QPolygonF &samples)
--- src/qwt/main.cpp
+++ src/qwt/main.cpp
@@ -3,7 +3,6 @@
 
 #include <qapplication.h>
 #include <iostream>
-#include <fstream>
 
 int main( int argc, char **argv )
 {
@@ -13,29 +12,11 @@ int main( int argc, char **argv )
   if (argc > 1)
     infile = std::string(argv[1]);
   
-  QApplication a( argc, argv );
+  QApplication a(argc, argv);
 
   MainWindow w(QString::fromStdString(infile));
   w.resize(800, 800);
   
-  // start reading input file
-  std::ifstream inputstream;
-  inputstream.open(infile.c_str());
-  if (!inputstream.is_open())
-    {
-      std::cerr << "error - could not open file "
-		<< infile << "\n";
-    }
-
-  // read points from file
-  float x, y;
-  int counter;
-  QPolygonF samples;
-  while (inputstream >> counter >> x >> y)
-    {
-      samples += QPointF( x, y );
-    }
-  w.setSamples(samples);
   w.show();
 
   return a.exec();
