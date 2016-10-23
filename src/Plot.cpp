ea323cda1ff5b2b40d30d7b8a5147f6aebfffb78
sjrdc <scrijns@gmail.com>
2016-10-23 21:54:31 +0200

    switch to qt5 compiled qwt version on linux; use hue colormap

--- src/Plot.cpp
+++ src/Plot.cpp
@@ -14,14 +14,12 @@ Plot::Plot(QWidget *parent) :
 {
   // attach curve
   curve = new QwtPlotSpectroCurve();
-  curve->setPenWidth(3);
+  curve->setPenWidth(5);
   curve->attach(this);
 
-#ifndef WIN32
-  colormap = new QwtLinearColorMap(Qt::yellow, Qt::red);
+  colormap = new QwtHueColorMap();
+  // ((QwtHueColorMap*)colormap)->setHueInterval(0, 360);
   curve->setColorMap(colormap);
-#else
-#endif
 
   setSymbol(NULL);
 
@@ -37,13 +35,9 @@ Plot::Plot(QWidget *parent) :
   grid->enableXMin(true);
   grid->enableYMin(true);
 
-#ifndef WIN32
-  grid->setMajPen(QPen(Qt::gray, 0, Qt::DotLine));
-  grid->setMinPen(QPen(Qt::gray, 0, Qt::DotLine));
-#else
   grid->setMajorPen(QPen(Qt::gray, 0, Qt::DotLine));
   grid->setMinorPen(QPen(Qt::gray, 0, Qt::DotLine));
-#endif
+
   grid->attach(this);
 }
 
@@ -59,16 +53,5 @@ void Plot::setSymbol(QwtSymbol *symbol)
 
 void Plot::setSamples(const QwtPoint3DSeriesData &samples)
 {
-  const QVector<QwtPoint3D> s = samples.samples();
-  curve->setSamples(s);
-  float mx = s.first().z();
-  float mi = mx;
-  for (QVector<QwtPoint3D>::const_iterator p = s.begin();
-       p != s.end(); ++p)
-    {
-      float z = p->z();
-      if (z > mx) mx = z;
-      else if (z < mi) mi = z;
-    }
-  curve->setColorRange(QwtInterval(0, mx));
+  curve->setSamples(samples.samples());
 }
