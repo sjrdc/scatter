8c84941715e62c93c052512d00f6b26d545f193c
sjrdc <scrijns@gmail.com>
2016-12-14 09:52:24 +0100

    split off smaller methods

--- src/PSFWindow.cpp
+++ src/PSFWindow.cpp
@@ -9,7 +9,7 @@
 
 size_t rowmajorindex(size_t r, size_t c, size_t rows, size_t cols)
 {
-  return (c + r*cols);
+    return (c + r*cols);
 }
 // org: #define rowmajorindex(i, j, dimx, dimy) (j + dimy*i)
 
@@ -51,56 +51,64 @@ void fft2shift(std::complex<float>* img, size_t rows, size_t cols)
 
 void PSFWindow::setSamples(const QVector<QwtPoint3D> &samples)
 {
-  QSize qsize = estimateSize(samples);
-  int rows = qsize.height();
-  int cols = qsize.width();
-  int size = std::max(rows, cols);
+    QSize qsize = estimateSize(samples);
+    int rows = qsize.height();
+    int cols = qsize.width();
     
-  std::complex<float>* F = new std::complex<float>[rows*cols];
-  memset(F, 0., rows*cols*sizeof(std::complex<float>));
-  for (int i = 0; i < samples.size(); ++i)
+    std::complex<float>* F = new std::complex<float>[rows*cols];
+    memset(F, 0., rows*cols*sizeof(std::complex<float>));
+    for (int i = 0; i < samples.size(); ++i)
     {
-      F[rowmajorindex((int)(samples[i].y() + rows/2),
-		      (int)(samples[i].x() + cols/2),
-		      rows, cols)] = 1;
+        F[rowmajorindex((int)(samples[i].y() + rows/2),
+                        (int)(samples[i].x() + cols/2),
+                        rows, cols)] = 1;
     }
-  std::complex<float>* f = new std::complex<float>[rows*cols];
+    std::complex<float>* f = new std::complex<float>[rows*cols];
 
-  fftwf_plan ifft2 = fftwf_plan_dft_2d(rows, cols,
-				       reinterpret_cast<fftwf_complex*>(F),
-				       reinterpret_cast<fftwf_complex*>(f),
-				       FFTW_BACKWARD, FFTW_ESTIMATE);
+    fftwf_plan ifft2 = fftwf_plan_dft_2d(rows, cols,
+                                         reinterpret_cast<fftwf_complex*>(F),
+                                         reinterpret_cast<fftwf_complex*>(f),
+                                         FFTW_BACKWARD, FFTW_ESTIMATE);
+    fft2shift(F, rows, cols);
+    fftwf_execute(ifft2);
+    fftwf_destroy_plan(ifft2);
 
-  fft2shift(F, rows, cols);
+    display(f, rows, cols);
 
-  fftwf_execute(ifft2);
-  fftwf_destroy_plan(ifft2);
+    delete [] f;
+    delete [] F;
 
-  QVector<double> out(size*size);
-  float minIntensity = std::abs(f[0]);
-  float maxIntensity = minIntensity;
+}
 
-  size_t rOffset = (size - rows)/2;
-  size_t cOffset = (size - cols)/2;
-  for (int r = rOffset; r < rOffset + rows; ++r)
-    for (int c = cOffset; c < cOffset + cols; ++c)
-      {
-	size_t iout = rowmajorindex(size - r, c, size, size);
-        out[iout] = std::abs(f[rowmajorindex(r - rOffset, c - cOffset, rows, cols)]);
-        if (out[iout] < minIntensity) minIntensity = out[iout];
-        else if (out[iout] > maxIntensity) maxIntensity = out[iout];
-      }
+void PSFWindow::display(std::complex<float>* f, size_t rows, size_t cols)
+{
+    int size = std::max(rows, cols);
+    QVector<double> out(size*size, 0.);
 
-  delete [] f;
-  delete [] F;
+    size_t rOffset = (size - rows)/2;
+    size_t cOffset = (size - cols)/2;
 
-  imagedisplay_->updateDisplay(out, size, size, minIntensity, maxIntensity);
+    float minIntensity = std::abs(f[0]);
+    float maxIntensity = minIntensity;
+
+    for (int r = rOffset; r < rOffset + rows; ++r)
+        for (int c = cOffset; c < cOffset + cols; ++c)
+        {
+            size_t iout = rowmajorindex(size - 1 - r, c, size, size);
+            out[iout] = std::abs(f[rowmajorindex(r - rOffset, c - cOffset, rows, cols)]);
+
+            if (out[iout] < minIntensity) minIntensity = out[iout];
+            else if (out[iout] > maxIntensity) maxIntensity = out[iout];
+        }
+
+    imagedisplay_->updateDisplay(out, size, size, minIntensity, maxIntensity);
 }
 
 QSize PSFWindow::estimateSize(const QVector<QwtPoint3D> &samples) const
 {
     QwtPoint3DSeriesData sd(samples);
     QRectF r = sd.boundingRect();
+
     int cols = 2*std::max(std::fabs(r.right()), std::fabs(r.left()));
     int rows = 2*std::max(std::fabs(r.top()), std::fabs(r.bottom()));
 
--- src/PSFWindow.h
+++ src/PSFWindow.h
@@ -2,19 +2,20 @@
 
 #include <QMainWindow>
 #include <qwt_point_3d.h>
+#include <complex>
 class ImageDisplay;
 
 class PSFWindow : public QMainWindow
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
     PSFWindow(QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = Qt::WindowFlags());
     void openFile(const QString &infile);
-  void setSamples(const QVector<QwtPoint3D> &samples);
+    void setSamples(const QVector<QwtPoint3D> &samples);
     void keyPressEvent(QKeyEvent *event);
-  
-protected:
-  QSize estimateSize(const QVector<QwtPoint3D> &samples) const;
 
-  ImageDisplay *imagedisplay_;
+private:
+    QSize estimateSize(const QVector<QwtPoint3D> &samples) const;
+    void display(std::complex<float>* f, size_t rows, size_t cols);
+    ImageDisplay *imagedisplay_;
 };
